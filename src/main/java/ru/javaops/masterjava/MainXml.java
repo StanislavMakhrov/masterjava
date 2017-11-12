a201c80cfc46897d77774159e2f0b6df1a052a00
SlavaM4@yandex.ru <SlavaM4@yandex.ru>
2017-11-12 18:18:07 +0300

    HW2_4

--- src/main/java/ru/javaops/masterjava/MainXml.java
+++ src/main/java/ru/javaops/masterjava/MainXml.java
@@ -1,4 +1,8 @@
 package ru.javaops.masterjava;
 
 public class MainXml {
+    public static void main(String[] args) {
+        System.out.format("Hello MasterJava!");
+        MainXml main = new MainXml();
+    }
 }
